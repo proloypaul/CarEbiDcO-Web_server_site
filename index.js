5b4186c39ad7e07f739a6407bea343a40d4d002a
proloypaul <proloyp23@gmail.com>
2021-11-10 14:53:56 +0600

    add get method

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const { MongoClient } = require('mongodb');
 const express = require('express')
+var cors = require('cors')
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 3800
+
+app.use(cors())
+app.use(express.json())
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.e3dsx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -11,7 +15,15 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
     try{
         await client.connect()
-        console.log("database connected successful")
+        const database = client.db('carbidcoWeb')
+        const productsCollection = database.collection('products')
+
+        // GET method to collect data from database 
+        app.get('/products', async (req, res) => {
+            const cursor = productsCollection.find({})
+            const result = await cursor.toArray()
+            res.json(result)
+        })
     }finally{
         // await client.close()
     }
