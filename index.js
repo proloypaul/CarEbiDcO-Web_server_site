98245d1d5c2f64c62db27904bde3a45ea5e36181
proloypaul <proloyp23@gmail.com>
2021-11-11 01:57:35 +0600

    user data send database

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { MongoClient } = require('mongodb');
 const express = require('express')
 var cors = require('cors')
 require('dotenv').config()
+const ObjectId = require('mongodb').ObjectId
 const app = express()
 const port = process.env.PORT || 3800
 
@@ -17,13 +18,31 @@ async function run(){
         await client.connect()
         const database = client.db('carbidcoWeb')
         const productsCollection = database.collection('products')
-
-        // GET method to collect data from database 
+        const usersCollection = database.collection('users')
+        // GET method to find all data from database 
         app.get('/products', async (req, res) => {
             const cursor = productsCollection.find({})
             const result = await cursor.toArray()
             res.json(result)
         })
+
+        // GET method to find one data from database using id
+        app.get('/products/:id', async (req, res) => {
+            const productId = req.params.id
+            // console.log("product id", productId)
+            const query = {_id: ObjectId(productId)}
+            const result = await productsCollection.findOne(query)
+            res.json(result)    
+        });
+
+        // POST method to user data insert in database
+        app.post('/users', async (req, res) => {
+            const userData = req.body 
+            // console.log("user data", userData)
+            const result = await usersCollection.insertOne(userData)
+            res.json(result)
+        })
+
     }finally{
         // await client.close()
     }
